8ca95534612090d1e93638fbf185eb2f11e1267f
artur12313 <arturpisula11@wp.pl>
2022-11-23 23:16:49 +0100

    Fixed bug with displaying booked days in calendar

--- resources/js/components/CalendarContent/BookedDays.jsx
+++ resources/js/components/CalendarContent/BookedDays.jsx
@@ -8,55 +8,28 @@ function BookedDays({ calendarBookings, room, daysToDisplay, actualMonth, actual
     let newbookingLink = '/booking'
 
     daysToCompare.forEach(day => {
-
-        calendarBookings.forEach(booking => {
-            let editLink = '/booking/details/' + booking.id;
-            if (booking.room_id === room.id && day >= booking.start_date && day <= booking.end_date) {
-                if (booking.start_date === day || booking.end_date === day) {
-                    console.log('booked days: ', day);
-                    // output.push(<a href={editLink} className="btn btn-sm btn-danger rounded-0 border outline-0 p-0" style={{width: 45}}><div className="py-1" style={{width: 45}}>&nbsp;</div></a>);
-                    output.push(<a href={editLink} className="btn btn-sm btn-danger rounded-0 border outline-0 p-0" style={{width: 45}}><div className="py-1" style={{width: 45}}>&nbsp;</div></a>);
-                }
-            }
-        })
-        console.log('not booked days: ', day);
-        output.push(<a href={newbookingLink} className="btn btn-sm btn-success rounded-0 border outline-0 p-0" style={{width: 45}}><div className="py-1">&nbsp;</div></a>);
-    })
-
-    // daysToCompare.filter(day => {
-    //     calendarBookings.filter(booking => booking.room_id === room.id && day >= booking.start_date && day <= booking.end_date).map(booking => {
-    //         output.push(<div className="bg-danger px-2 py-1 border">&nbsp;</div>);
-    //     })
-    // })
-
-    let days = daysToCompare.map(day => (
-        <div key={day} className="border-right d-flex flex-wrap text-center px-2 py-1">
-            <div className="d-flex flex-wrap justify-content-center">
-                <div>{day}</div>
-                {BookedDays}
-            </div>
-        </div>
-    ))
-
-
+        let bookings = calendarBookings.filter(booking => booking.room_id === room.id && day >= booking.start_date && day <= booking.end_date);
+        if (bookings.length === 0) {
+            output.push(
+                <div key={day}>
+                    <a href={newbookingLink} className="btn btn-sm btn-success rounded-0 border outline-0 p-0" style={{width: 45}}><div className="py-3">&nbsp;</div></a>
+                </div>
+            );
+        } else {
+            bookings.forEach(booking => {
+                let editLink = '/booking/details/' + booking.id;
+                output.push(
+                    <div key={day}>
+                       <a href={editLink} className="btn btn-sm btn-danger rounded-0 border outline-0 p-0" style={{width: 45}}><div className="py-3" style={{width: 45}}>&nbsp;</div></a>
+                    </div>
+                );
+            });
+        }
+    });
 
     return (
-        <div className="col-9 px-0 monthDays d-flex flex-wrap justify-content-around" id="days">
-            {/* {daysToCompare.map(day => (
-                <div key={day} className="border-right d-flex flex-wrap text-center px-2 py-1">
-                    <div className="d-flex flex-wrap justify-content-center">
-                        <div>{day}</div>
-                        {calendarBookings.filter(booking => booking.room_id === room.id).map(booking => (
-                            <div key={booking.id}>
-                                <li>{booking.start_date}</li>
-                                <li>{booking.end_date}</li>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            ))} */}
-
-            {output.map(day => day)}
+        <div className="col-9 px-0 monthDays d-flex flex-wrap justify-content-around">
+                   {output}
         </div>
     );
 }
--- resources/js/components/CalendarContent/CalendarContent.jsx
+++ resources/js/components/CalendarContent/CalendarContent.jsx
@@ -7,27 +7,27 @@ function CalendarContent({ calendarSubcategories, calendarRooms, category, calen
         <div className="card-body p-0">
             <div className="col-12 px-0">
 
-                {calendarSubcategories.filter(sub => sub.parent_id === category.id) != null ? (
+                {calendarSubcategories.filter(sub => sub.parent_id === category.id).lenght !== 0 ? (
                     calendarSubcategories.filter(sub => sub.parent_id === category.id).map(sub => (
                         <div key={sub.id}>
                             <CalendarContentTop calendarSubcategories={sub} daysToDisplay={daysToDisplay} actualMonth={actualMonth}/>
                                 {calendarRooms.filter(item => item.category_id === sub.id).map(item => (
-                                    <div key={item.id} className="col-12 px-0 border-bottom d-flex overflow-scroll">
+                                    <div key={item.id} className="col-12 px-0 border-bottom d-flex">
                                         <div className="col-3 border-right px-2 py-3 d-flex align-items-center justify-content-center">
                                             <h5 className="text-uppercase font-weight-bold mb-0 text-center">pokój: {item.name}</h5>
                                         </div>
-                                        <BookedDays calendarBookings={calendarBookings} room={item} daysToDisplay={daysToDisplay} daysToCompare={daysToCompare}/>
+                                        <BookedDays calendarBookings={calendarBookings} room={item} daysToDisplay={daysToDisplay} actualMonth={actualMonth} actualYear={actualYear} daysToCompare={daysToCompare}/>
                                     </div>
                                 ))}
                         </div>
                     ))
                 ) : (
                     <>
-                        <BookedDays calendarBookings={calendarBookings} room={item} daysToDisplay={daysToDisplay} daysToCompare={daysToCompare}/>
+                        <BookedDays calendarBookings={calendarBookings} room={room} daysToDisplay={daysToDisplay} actualMonth={actualMonth} actualYear={actualYear} daysToCompare={daysToCompare}/>
                     </>
                 )}
                     <>
-                        {/* <CalendarContentTop daysToDisplay={daysToDisplay} actualMonth={actualMonth}/> */}
+                        <CalendarContentTop daysToDisplay={daysToDisplay} actualMonth={actualMonth}/>
                         {calendarRooms.filter(room => room.category_id === category.id).map(room => (
                             <div key={room.id} className="col-12 px-0 border-bottom d-flex">
                                 <div className="col-3 border-right px-2 py-3 d-flex align-items-center justify-content-center">
